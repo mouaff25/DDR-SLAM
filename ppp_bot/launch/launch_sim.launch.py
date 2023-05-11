887392f8f3fb4ba573e21edba71fddb92742921d
Mouafak Dakhlaoui <mouaffak9@gmail.com>
2023-05-11 07:38:57 +0100

    Updated launch file + removed executables

--- ppp_bot/launch/launch_sim.launch.py
+++ ppp_bot/launch/launch_sim.launch.py
@@ -4,9 +4,11 @@ from ament_index_python.packages import get_package_share_directory
 
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, RegisterEventHandler, LogInfo
+from launch.actions import IncludeLaunchDescription, RegisterEventHandler, LogInfo, ExecuteProcess, EmitEvent
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.event_handlers import OnProcessStart
+from launch.event_handlers import OnProcessStart, OnExecutionComplete, OnProcessExit
+from launch.substitutions import EnvironmentVariable
+from launch.events import Shutdown
 
 from launch_ros.actions import Node
 import xacro
@@ -48,34 +50,95 @@ def generate_launch_description():
     )
 
 
-    node_ignition_launcher_params = {'world_file': world_file}
-    node_ignition_launcher = Node(
-        package=package_name,
-        executable='ignition_launcher',
-        parameters=[node_ignition_launcher_params]
+    ignition_launcher_params = {'world_file': world_file}
+
+    ignition_launcher = ExecuteProcess(
+        cmd=[['ign gazebo ',
+            ignition_launcher_params['world_file']]],
+        shell=True
     )
 
-    node_urdf_spawner_params = {'service': '/world/empty_ppp/create',
+    urdf_spawner_params = {'service': '/world/empty_ppp/create',
                                 'sdf_filename': urdf_file,
                                 'name': 'ppp_robot'}
-    node_urdf_spawner = Node(
-        package=package_name,
-        executable='urdf_spawner',
-        parameters=[node_urdf_spawner_params]
+
+    urdf_spawner = ExecuteProcess(
+        cmd=[['ign service -s ',
+           urdf_spawner_params['service'],
+            ' --reqtype ignition.msgs.EntityFactory --reptype ignition.msgs.Boolean ',
+            '--timeout 1000 --req \'sdf_filename: "',
+            urdf_spawner_params['sdf_filename'],
+            '", name: "',
+            urdf_spawner_params['name'],
+            '", pose: {position: {z: 1.0}}\'']],
+        shell=True
+    )
+
+    node_parameter_bridge_args = ['/cmd_vel@geometry_msgs/msg/Twist@ignition.msgs.Twist',
+                                '/lidar@sensor_msgs/msg/LaserScan[ignition.msgs.LaserScan']
+    node_parameter_bridge = Node(
+        package='ros_ign_bridge',
+        executable='parameter_bridge',
+        arguments=node_parameter_bridge_args,
+        output='screen'
+    )
+
+
+    node_teleop = Node(
+        package="teleop_twist_keyboard",
+        executable="teleop_twist_keyboard",
+        prefix='xterm -e',
+        output='screen',
     )
 
     # Launch!
     return LaunchDescription([
+
         node_robot_state_publisher,
+
         node_joint_state_publisher,
-        node_ignition_launcher,
+
+        ignition_launcher,
+
         RegisterEventHandler(
             OnProcessStart(
-                target_action=node_ignition_launcher,
+                target_action=ignition_launcher,
                 on_start=[
                     LogInfo(msg='Simulation launched, spawning urdf...'),
-                    node_urdf_spawner
+                    urdf_spawner
                 ]
             )
-        )
+        ),
+
+        RegisterEventHandler(
+            OnExecutionComplete(
+                target_action=urdf_spawner,
+                on_completion=[
+                    LogInfo(msg='Spawn finished, establishing parameter_bridge'),
+                    node_parameter_bridge
+                ]
+            )
+        ),
+
+        RegisterEventHandler(
+            OnProcessStart(
+                target_action=node_parameter_bridge,
+                on_start=[
+                    LogInfo(msg='Running teleop_twist_keyboard...'),
+                    node_teleop
+                ]
+            )
+        ),
+
+        RegisterEventHandler(
+            OnProcessExit(
+                target_action=ignition_launcher,
+                on_exit=[
+                    LogInfo(msg=(EnvironmentVariable(name='USER'),
+                            ' closed the ignition window')),
+                    EmitEvent(event=Shutdown(
+                        reason='Window closed'))
+                ]
+            )
+        ),
     ])
