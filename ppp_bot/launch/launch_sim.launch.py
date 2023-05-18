c38d6527843ba3c39883143a895288781fcb9dc1
Mouafak Dakhlaoui <mouaffak9@gmail.com>
2023-05-18 20:27:08 +0100

    Used ros_ign_gazebo package to launch Ignition

--- ppp_bot/launch/launch_sim.launch.py
+++ ppp_bot/launch/launch_sim.launch.py
@@ -24,11 +24,6 @@ def generate_launch_description():
 
     package_name='ppp_bot'
 
-    env = {
-        'IGN_GAZEBO_SYSTEM_PLUGIN_PATH': ':'.join([
-            os.environ.get('IGN_GAZEBO_SYSTEM_PLUGIN_PATH', default=''),
-            os.environ.get('LD_LIBRARY_PATH', default='')]
-        )}
 
     # Check if we're told to use sim time
 
@@ -50,42 +45,40 @@ def generate_launch_description():
         parameters=[node_robot_state_publisher_params]
     )
 
-    node_joint_state_publisher = Node(
-        package='joint_state_publisher',
-        executable='joint_state_publisher'
-    )
 
 
     ignition_launcher_params = {'world_file': world_file}
 
-    ignition_launcher = ExecuteProcess(
-        cmd=[['ign gazebo ',
-            ignition_launcher_params['world_file']]],
-        additional_env=env,
-        shell=True
-    )
 
-    urdf_spawner_params = {'service': '/world/empty_ppp/create',
+    ignition_launcher = IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                [os.path.join(get_package_share_directory('ros_ign_gazebo'),
+                              'launch', 'ign_gazebo.launch.py')]),
+            launch_arguments=[('gz_args', [f' -r -v 4 {ignition_launcher_params["world_file"]}'])])
+
+    ignition_spawn_entity_params = {'service': '/world/empty_ppp/create',
                                 'sdf_filename': urdf_file,
                                 'name': 'ppp_robot'}
 
-    urdf_spawner = ExecuteProcess(
+    ignition_spawn_entity = ExecuteProcess(
         cmd=[['ign service -s ',
-           urdf_spawner_params['service'],
+           ignition_spawn_entity_params['service'],
             ' --reqtype ignition.msgs.EntityFactory --reptype ignition.msgs.Boolean ',
             '--timeout 1000 --req \'sdf_filename: "',
-            urdf_spawner_params['sdf_filename'],
+            ignition_spawn_entity_params['sdf_filename'],
             '", name: "',
-            urdf_spawner_params['name'],
+            ignition_spawn_entity_params['name'],
             '", pose: {position: {z: 1.0}}\'']],
         shell=True
     )
 
 
-    node_parameter_bridge_args = ['/cmd_vel@geometry_msgs/msg/Twist@ignition.msgs.Twist',
+    node_parameter_bridge_args = [
+                                # '/cmd_vel@geometry_msgs/msg/Twist@ignition.msgs.Twist',
                                 '/lidar@sensor_msgs/msg/LaserScan[ignition.msgs.LaserScan',
                                 '/world/empty_ppp/model/ppp_robot/link/base_link/sensor/camera/image@sensor_msgs/msg/Image[ignition.msgs.Image',
-                                '/world/empty_ppp/model/ppp_robot/link/base_link/sensor/camera/camera_info@sensor_msgs/msg/CameraInfo[ignition.msgs.CameraInfo']
+                                '/world/empty_ppp/model/ppp_robot/link/base_link/sensor/camera/camera_info@sensor_msgs/msg/CameraInfo[ignition.msgs.CameraInfo',
+                                '/clock@rosgraph_msgs/msg/Clock[ignition.msgs.Clock']
 
     node_parameter_bridge = Node(
         package='ros_gz_bridge',
@@ -98,32 +91,31 @@ def generate_launch_description():
     node_teleop = Node(
         package="teleop_twist_keyboard",
         executable="teleop_twist_keyboard",
+        remappings=[('/cmd_vel', 'diff_drive_base_controller/cmd_vel_unstamped')],
         prefix='xterm -e',
         output='screen',
+    )
+
+
+
+    load_joint_state_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+             'joint_state_broadcaster'],
+        output='screen'
+    )
+
+    load_joint_trajectory_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+             'diff_drive_base_controller'],
+        output='screen'
     )
 
     # Launch!
     return LaunchDescription([
 
-        node_robot_state_publisher,
-
-        node_joint_state_publisher,
-
-        ignition_launcher,
-
-        RegisterEventHandler(
-            OnProcessStart(
-                target_action=ignition_launcher,
-                on_start=[
-                    LogInfo(msg='Simulation launched, spawning urdf...'),
-                    urdf_spawner
-                ]
-            )
-        ),
-
         RegisterEventHandler(
             OnExecutionComplete(
-                target_action=urdf_spawner,
+                target_action=ignition_spawn_entity,
                 on_completion=[
                     LogInfo(msg='Spawn finished, establishing parameter_bridge'),
                     node_parameter_bridge
@@ -142,14 +134,20 @@ def generate_launch_description():
         ),
 
         RegisterEventHandler(
-            OnProcessExit(
-                target_action=ignition_launcher,
-                on_exit=[
-                    LogInfo(msg=(EnvironmentVariable(name='USER'),
-                            ' closed the ignition window')),
-                    EmitEvent(event=Shutdown(
-                        reason='Window closed'))
-                ]
+            event_handler=OnProcessExit(
+                target_action=ignition_spawn_entity,
+                on_exit=[load_joint_state_controller],
             )
         ),
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=load_joint_state_controller,
+                on_exit=[load_joint_trajectory_controller],
+            )
+        ),
+        ignition_launcher,
+
+        node_robot_state_publisher,
+        ignition_spawn_entity
+        
     ])
